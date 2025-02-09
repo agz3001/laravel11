d5afc7a0322242cc27e3c4b1495e46bd17f0ef65
agz3001 <agz3001@yahoo.co.jp>
2025-02-09 19:02:41 +0900

    schedulerの変更

--- routes/console.php
+++ routes/console.php
@@ -5,13 +5,19 @@ use Illuminate\Support\Facades\Artisan;
 // 追加
 use Illuminate\Support\Facades\Schedule;
 
+// 呼び出し: php artisan inspire
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
 // 追加
+// 呼び出し: php artisan schedule:run
 Schedule::call(function(){
+    logger()->info('write to logger...');
     dd('こんにちは');
-})->everySecond();
+})->everyFiveSeconds();
 // 自作commandによる別の書き方
-Schedule::command('app:greetings')->everySecond();
+// 呼び出し: php artisan app:greetings
+Schedule::command('app:greetings')->everyFiveSeconds();
+
+// docker compose logs -f app でログを確認
